96d5797a2a49e26cd0ebe9733d1f1315551c6e39
Jussi Torkko <49977677+Torakka@users.noreply.github.com>
2021-12-30 16:10:23 +0200

    Added: - game stats - more collision detection - limited ships - game active flag

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,10 @@
 import sys
+from time import sleep
 
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -18,6 +20,8 @@ class AlienInvasion:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+        # Create an instance to store game statistics.
+        self.stats = GameStats(self)
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
@@ -28,9 +32,10 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
             self._update_screen()
 
             
@@ -79,12 +84,29 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""    
+        # Remove any bulletts and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if not self.aliens:
+            # Destry existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
     
     def _update_aliens(self):
         """Check if the fleet is at an edge,
         then update the position of all aliens in the fleet."""
         self._check_fleet_edges()
         self.aliens.update()
+
+        # Look for alien-ship collision.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            self._ship_hit()
+        
+        # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -124,6 +146,31 @@ class AlienInvasion:
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Treat this the same as if  the ship got hit.
+                self._ship_hit()
+                break
+
+    def _ship_hit(self):
+        """Respond to the ship being hit by alien."""
+        if self.stats.ships_left > 0:
+            # Decrement shps_left.
+            self.stats.ships_left -= 1
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
+            #Create a new fleet of aliens and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
+            # Pause.
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
--- settings.py
+++ settings.py
@@ -9,18 +9,19 @@ class Settings:
         self.bg_colour = (230, 230, 230)
 
         # Ship settings.
-        self.ship_speed = 5.5 # original setting 1.5
+        self.ship_speed = 10 # original setting 1.5
+        self.ship_limit = 3
 
         # Bullet settings.
         self.bullet_speed = 10.0 # original setting 1.0
-        self.bullet_width = 3
+        self.bullet_width = 1000 # original settings 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
         # Alien settings.
-        self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.alien_speed = 10.0 # original setting 1.0
+        self.fleet_drop_speed = 100 # origilan setting 10
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
