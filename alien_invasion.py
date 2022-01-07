d19a34b28cc475f357496672721c98f25dfb3283
Jussi Torkko <jussitorkko79@gmail.com>
2022-01-07 21:00:35 +0200

    Implemented increasing game speed.

--- alien_invasion.py
+++ alien_invasion.py
@@ -59,6 +59,8 @@ class AlienInvasion:
         """Start a new game when player clicks the Play button."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
             # Reset the stats.
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -115,6 +117,7 @@ class AlienInvasion:
             # Destry existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
     
     def _update_aliens(self):
         """Check if the fleet is at an edge,
