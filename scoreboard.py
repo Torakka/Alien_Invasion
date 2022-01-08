8319828673fc973f566b3a9513408ee9f78559a1
Jussi Torkko <49977677+Torakka@users.noreply.github.com>
2022-01-08 18:57:14 +0200

    Fix typo in bg_colour and changed settings for speed suitable for testing.

--- scoreboard.py
+++ scoreboard.py
@@ -22,7 +22,7 @@ class Scoreboard:
         rounded_score = round(self.stats.score, -1)
         score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True,
-            self.text_color, self.settings.bg_color)
+            self.text_color, self.settings.bg_colour)
         # Display the score at the top right of the screen.
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
@@ -37,7 +37,7 @@ class Scoreboard:
         """Turn the high score into a rendered image."""
         high_score = round(self.stats.high_score, -1)
         high_score_str = "{:,}".format(high_score)
-        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_colour)
         # Center the high score at the top of the screen.
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
--- settings.py
+++ settings.py
@@ -32,7 +32,7 @@ class Settings:
         """Initialize settings that change throughout the game."""
         self.ship_speed = 10 # original setting 1.5
         self.bullet_speed = 10.0 # original setting 1.0
-        self.alien_speed = 1.0 # original setting 1.0
+        self.alien_speed = 10.0 # original setting 1.0
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
         # Scoring
