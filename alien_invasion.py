cb8e5be5811084d77dcac010ea56b6e29b0644b5
Jussi Torkko <49977677+Torakka@users.noreply.github.com>
2022-01-26 21:05:41 +0200

    Finalizing scoreboard etc.

--- alien_invasion.py
+++ alien_invasion.py
@@ -67,6 +67,8 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
             self.bullets.empty()
@@ -126,6 +128,9 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
     
     def _update_aliens(self):
         """Check if the fleet is at an edge,
@@ -191,8 +196,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by alien."""
         if self.stats.ships_left > 0:
-            # Decrement shps_left.
+            # Decrement shps_left, and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
             self.bullets.empty()
