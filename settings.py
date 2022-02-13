16c8cee71221972cddff67bab33cf2483dca35c2
Jussi Torkko <jussitorkko79@gmail.com>
2022-02-13 15:55:16 +0200

    Reseting settings once got it working with mac.

--- settings.py
+++ settings.py
@@ -15,13 +15,13 @@ class Settings:
         self.bullet_width = 3 # original settings 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 10
+        self.bullets_allowed = 10 # orginal setting 3
 
         # Alien settings.   
         self.fleet_drop_speed = 10 # original setting 10
         
         # How quickly the game speeds up
-        self.speedup_scale = 1.2 # original setting 1.1
+        self.speedup_scale = 1.1 # original setting 1.1
 
         # How quickly the alien point values increase.
         self.score_scale = 1.5
@@ -30,9 +30,9 @@ class Settings:
 
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
-        self.ship_speed = 10 # original setting 1.5
-        self.bullet_speed = 10.0 # original setting 1.0
-        self.alien_speed = 5.0 # original setting 1.0
+        self.ship_speed = 1.5 # original setting 1.5
+        self.bullet_speed = 1.0 # original setting 1.0
+        self.alien_speed = 1.0 # original setting 1.0
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
         # Scoring
